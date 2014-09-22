2c6eaca8f6c2a540a29f98483e0c1971f37af6ff
James Rickard <james@frodosghost.com>
2014-09-22 10:03:16 +1000

    Updated bootbox to work with updated layout changes in formatting.

--- Resources/public/js/bootbox.js
+++ Resources/public/js/bootbox.js
@@ -355,7 +355,7 @@ var bootbox = window.bootbox || (function(document, $) {
         // @see https://github.com/twitter/bootstrap/issues/4854
         // for an explanation of tabIndex=-1
 
-        var parts = ["<div class='bootbox modal' tabindex='-1' style='overflow:hidden;'>"];
+        var parts = ["<div class='bootbox modal' tabindex='-1' style='overflow:hidden;'><div class='modal-dialog'><div class='modal-content'>"];
 
         if (options['header']) {
             var closeButton = '';
@@ -373,7 +373,7 @@ var bootbox = window.bootbox || (function(document, $) {
             parts.push("<div class='modal-footer'>"+buttons+"</div>");
         }
 
-        parts.push("</div>");
+        parts.push("</div><!-- /.modal-content --></div><!-- /.modal-dialog --></div>");
 
         var div = $(parts.join("\n"));
 
